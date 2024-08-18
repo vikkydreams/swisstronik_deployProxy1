6ba7c36f6bdf12ae46977d464d5f884e90e4c15f
sleepy wiz <abikoyetosin63@gmail.com>
2024-08-18 10:57:34 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0xAd3F0A931Ce04993d2302C2B2F4fc5155dDe4376'
+export const ProxyAdmin = '0x9794Cb56CDc71985297D15eEbFD8F32DD6Dc071a'
+export const SWTRImplementation = '0x5d010152311d50973EB0f7a30746a2688a90FbA4'
     
